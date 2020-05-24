9e1975bfdedc1d04d01eeca4f1a710f67ee36572
Konstantin Suvorov <47966800+k-a-suvorov@users.noreply.github.com>
2020-05-24 23:45:23 +0300

    Add files via upload
    Simple telegram bot

---
+++ opsistalkbot.py
@@ -0,0 +1,12 @@
+import telebot
+bot = telebot.TeleBot("1261022095:AAFi-m_hIj4l43-o6xdJ_Ioc3L4BwdGsCPs")
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, "Howdy, how are you doing?")
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+	bot.reply_to(message, message.text)
+
+bot.polling(none_stop=True)
